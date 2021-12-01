267f588a676bf63c7c588e9ab3b91da68ecbd634
FIRST_NAME LAST_NAME <soulayma97715@gmail.com>
2021-12-01 21:21:20 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+//DOM
+const touches = [...document.querySelectorAll('.button')];
+const listeKeycode = touches.map(touche => touche.dataset.key);
+const ecran = document.querySelector('.ecran');
+document.addEventListener('keydown',(e) => {
+    const valeur = e.KeyCode.toSring();
+    calculer(valeur);
+})
+document.addEventListener('click', (e) => {
+    const valeur = e.target.dataset.key;
+    calculer(valeur);
+})
+const calculer = (valeur) => {
+    if (listeKeycode.includes(valeur)){
+        switch(valeur){
+            case '8':
+                ecran.textContent = "";
+                break;
+            case '13':
+                const cacul = eval(ecran.textContent);
+                ecran.textContent = cacul;
+                break;
+            default:
+                const indexKeycode = listeKeycode.indexOf(valeur);
+                const touche = touches[indexKeycode];
+                ecran.textContent += touche.innerHTML;        
+        }
+    }
+}
+window.addEventListener('error', (e) => {
+    alert('Une erreur est survenue dans votre calcul : '+ e.message)
+})
